5642335112e976f75440ef17efc3c3cadee6bc24
Gregory <gregpappas9@gmail.com>
2017-03-10 23:31:02 -0600

    v1 commit

---
+++ Youtube-Bot/src/listeners/reconnect.js
@@ -0,0 +1,15 @@
+/* 
+
+const { Listener } = require("discord-akairo");
+
+function exec() {
+    return console.log("Attempting to reconnect...");
+}
+
+module.exports = new Listener("reconnecting", exec, {
+    emitter: "client",
+    eventName: "reconnecting",
+    type: "on"
+});
+
+*/
