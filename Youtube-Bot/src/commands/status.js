438f22dfcefc30ff5f485a872fe3350cf2f999e2
Gregory <gregpappas9@gmail.com>
2017-03-11 17:04:10 -0600

    Update status.js

--- Youtube-Bot/src/commands/status.js
+++ Youtube-Bot/src/commands/status.js
@@ -3,13 +3,13 @@ const { Command } = require("discord-akairo");
 const config = require("./../../config.json");
      
 function exec(message, args) {
-    if (!args.status) return message.channel.sendEmbed(this.client.util.embed().setAuthor(`${config.prefix}status`).setDescription("Changes the bots status. Available statuses are **online, dnd, idle, and invisible**").setColor(14493733));
+    if (!args.toSet) return message.channel.sendEmbed(this.client.util.embed().setAuthor(`${config.prefix}status`).setDescription("Changes the bots status. Available statuses are **online, dnd, idle, and invisible**").setColor(14493733));
     this.client.user.setStatus(args.toSet);
     return message.delete().catch(e => {}); // the message getting deleted is optional so I don't want the error going to the console :eyes:
 }
 
 module.exports = new Command("status", exec, {
-    aliases: ["status"]
+    aliases: ["status", "setstatus"]
     , args: [{
         id: "toSet"
         , type: ["online", "idle", "dnd", "invisible"]
